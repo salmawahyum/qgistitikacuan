124e4d4fcdefa78d2c7527cdb6796affeeee62cc
salmawahyum <76051240+salmawahyum@users.noreply.github.com>
2020-12-16 00:01:52 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.408453889776695, -6.983470330760185 ] } }
+]
+}
